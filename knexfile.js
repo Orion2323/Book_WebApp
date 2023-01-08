bdc362579bfb94b20d8b2ec954d1f46bdbc8d26e
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-01-07 22:35:03 -0600

    Implement knex into project

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+    development: {
+        client: 'mysql',
+        debug: true,
+
+        connection: {
+            host: 'host',
+            port: 0000,
+            user: 'user',
+            password: 'password',
+            insecureAuth: true,
+            database: 'db'
+        }
+    }
+};
