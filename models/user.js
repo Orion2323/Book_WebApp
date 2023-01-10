d13131b22acd736f331a1296a0cf962362e51443
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-01-10 01:43:49 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,20 +4,20 @@ const USER_TABLE = 'users';
 
 // function for inserting new user information into database
 const createNewUser = async (firstName, lastName, email, salt, password) => {
-    const result = await knex(USER_TABLE).insert({firstName, lastName, email, salt, password});
-    return result;
-}
-
-// function for finding user by email into database
-const findUserByEmail = async (email) => {
-    const result = await knex(USER_TABLE).where({email});
-    return result;
+    const results = await knex(USER_TABLE).insert({firstName, lastName, email, salt, password});
+    return results;
 }
 
 // function for deleting user from database
 const deleteUser = async (email) => {
-    const result = await knex(USER_TABLE).where({email}).del();
-    return result;
+    const results = await knex(USER_TABLE).where({email}).del();
+    return results;
+}
+
+// function for finding user by email into database
+const findUserByEmail = async (email) => {
+    const results = await knex(USER_TABLE).where({email});
+    return results;
 }
 
 module.exports = {
