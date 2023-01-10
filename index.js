2e5c9e319aa6e936572d991f2320347fcee6d886
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-01-09 22:22:46 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Import route handlers
+const userRoutes = require('./routes/user');
+const sessionRoutes = require('./routes/session');
+
+// Import middleware
+const {createMiddleware} = require('./middleware/model-middleware.js');
+const {authenticateJWT} = require('./middleware/auth');
+
+// Define express app instance
+const app = express();
+const port = 3000;
+
+app.use(createMiddleware);
+app.use(bodyParser.json());
+
+// health route to verify that connection to database is active
+app.get('/health', (req, res, next) => {
+    const responseBody = {
+        status: 'up',
+        port
+    };
+
+    res.json(responseBody);
+    next();
+});
+
+app.use('/session', sessionRoutes);
+app.use('/user', userRoutes, authenticateJWT);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
